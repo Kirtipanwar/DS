7aa1d9ac1a3d4aef888ecc2f55f2bbe1d4080759
Kirti Panwar <kirti09051999@ducic.ac.in>
2021-03-11 08:52:46 +0530

    Create getMazePaths.java

---
+++ recursion&Backtracking/recursionWithArraylist/getMazePaths.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scn = new Scanner(System.in);
+        
+        int nr = scn.nextInt(); 
+        int nc = scn.nextInt();
+        
+        System.out.println(getMazePaths(0,0,nr-1,nc-1));
+
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    
+    public static ArrayList < String > getMazePaths(int sr, int sc, int dr, int dc) {
+        
+        if(sr>dr || sc>dc){
+            return new ArrayList <String> ();
+        }
+        
+        if(sr==dr && sc==dc){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+        
+        
+        ArrayList<String> hPaths = getMazePaths(sr,sc+1,dr,dc);
+        ArrayList<String> vPaths = getMazePaths(sr+1,sc,dr,dc);
+        
+        ArrayList<String> myList = new ArrayList<>();
+        
+        for(String path: hPaths){
+            myList.add("h"+path);
+        }
+        
+        for(String path : vPaths){
+            myList.add("v"+path);
+        }
+        
+        return myList;
+    }
+
+}
