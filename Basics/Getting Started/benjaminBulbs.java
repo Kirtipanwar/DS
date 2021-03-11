1f7b9b7aaf3a4fefddcc27d804c98d3511f0d5a2
Kirti Panwar <kirti09051999@ducic.ac.in>
2021-03-11 07:57:16 +0530

    Create benjaminBulbs.java

---
+++ Basics/Getting Started/benjaminBulbs.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        
+        Scanner scn = new Scanner(System.in);
+        
+        int n = scn.nextInt();
+        
+        for(int fac=1; fac*fac<=n; fac++){
+            System.out.println(fac*fac);
+        }
+    }
+}
