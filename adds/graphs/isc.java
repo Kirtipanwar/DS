ee97fed1e4864f7e4565adf410c05b2cb98fe7f6
kirtipanwar <42113400+kirtipanwar@users.noreply.github.com>
2021-05-28 22:35:46 +0530

    Update isc.java

--- adds/graphs/isc.java
+++ adds/graphs/isc.java
@@ -5,76 +5,46 @@ public class Main {
    static class Edge {
       int src;
       int nbr;
+      int wt;
 
-      Edge(int src, int nbr) {
+      Edge(int src, int nbr, int wt) {
          this.src = src;
          this.nbr = nbr;
+         this.wt = wt;
       }
    }
-   public static class Pair{
-       int vtx;
-       String psf;
-       
-       Pair(int vtx, String psf){
-           this.vtx = vtx;
-           this.psf = psf;
-       }
-   }
-   public static void bfs(ArrayList<Edge>[] graph, int src){
-       Queue<Pair> queue = new ArrayDeque<>();
-       queue.add(new Pair(src,""+src));
-       boolean visited[] = new boolean[graph.length];
-       while(queue.size()>0){
-           Pair fpair = queue.remove();
-           
-           if(visited[fpair.vtx]==false){
-               visited[fpair.vtx]=true;
-               System.out.println(fpair.vtx+"@"+fpair.psf);
-               
-               for(Edge e : graph[fpair.vtx]){
-                   if(visited[e.nbr]==false){
-                       queue.add(new Pair(e.nbr,fpair.psf+e.nbr));
-                   }
-               }
-           }
-       }
-   }
-   
-   public static boolean isGraphCyclic(ArrayList<Edge>[] graph){
-       boolean visited[] = new boolean [graph.length];
-       for(int v=0; v<graph.length; v++){
-           if(visited[v]==false){
-               boolean res = isCompCyclic(graph, v, visited);
-               if(res==true){
-                   return true;
-               }
-           }
-       }
-       return false;
-   }
-   
-   public static boolean isCompCyclic(ArrayList<Edge>[] graph, int src, boolean visited[]){
-       Queue<Integer> queue = new ArrayDeque<>();
-       
-       queue.add(src);
-       while(queue.size()>0){
-           int fvtx = queue.remove();
-           
-           if(visited[fvtx]==false){
-               visited[fvtx] = true;
-               
-               for(Edge e : graph[fvtx]){
-                   if(visited[e.nbr]==false){
-                       queue.add(e.nbr);
-                   }
-               }
-           }else{
-               return true;
-           }
-       }
-       return false;
-   }
   
+    public static boolean isGraphCyclic(ArrayList<Edge>[] graph){
+    boolean visited[] = new boolean [graph.length];
+    for(int v = 0 ; v < graph.length ; v++){
+        if(visited[v] == false){
+            boolean res = isCompCyclic(graph, v,visited);
+            if(res == true){
+                return true;
+            }
+        }
+    }
+    return false;
+  }
+  public static boolean isCompCyclic(ArrayList<Edge>[] graph,int src,boolean visited[]){
+    Queue<Integer> queue = new ArrayDeque<>();
+    queue.add(src);
+    while(queue.size() > 0){
+      int fvtx = queue.remove();
+      if(visited[fvtx] == false){
+          visited[fvtx] = true;          
+          for(Edge e : graph[fvtx]){
+              if(visited[e.nbr] == false){
+                  queue.add(e.nbr);
+              }
+          }
+      }else{
+          // vtx is already visited --> more than one path exists , return true
+          return true;
+      }
+    }
+    return false;
+  }
 
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -90,11 +60,10 @@ public class Main {
          String[] parts = br.readLine().split(" ");
          int v1 = Integer.parseInt(parts[0]);
          int v2 = Integer.parseInt(parts[1]);
-         graph[v1].add(new Edge(v1, v2));
-         graph[v2].add(new Edge(v2, v1));
+         int wt = Integer.parseInt(parts[2]);
+         graph[v1].add(new Edge(v1, v2, wt));
+         graph[v2].add(new Edge(v2, v1, wt));
       }
-
-      //fun call 
-      
+      System.out.println(isGraphCyclic(graph));
    }
 }
