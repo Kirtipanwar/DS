4ee7c82623904c3340e5816b62d78dd50839bf11
kirtipanwar <42113400+kirtipanwar@users.noreply.github.com>
2021-06-01 20:39:33 +0530

    Update ab.java

--- adds/greedyDP/ab.java
+++ adds/greedyDP/ab.java
@@ -21,5 +21,29 @@ public static void main(String[] args) throws Exception {
     total = total * total;
     System.out.println(total);
  }
+}
+//---------------------------------------------------for long range constraints----------------------------
+import java.io.*;
+import java.util.*;
 
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    long n = scn.nextInt();
+    
+    long ob = 1;
+    long os = 1;
+    
+    for(int i = 2; i<= n; i++){
+        long nb = os;
+        long ns = os + ob;
+        
+        os = ns;
+        ob = nb;
+    }
+    long total = os + ob;
+    total = total * total;
+    System.out.println(total);
+ }
 }
