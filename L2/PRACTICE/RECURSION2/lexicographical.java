d77e663af4bba46308f51e58958280f93f5e3929
kirtipanwar <42113400+kirtipanwar@users.noreply.github.com>
2021-06-23 18:53:55 +0530

    Update lexicographical.java

--- L2/PRACTICE/RECURSION2/lexicographical.java
+++ L2/PRACTICE/RECURSION2/lexicographical.java
@@ -1,1 +1,23 @@
+import java.io.*;
+import java.util.*;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		
+		for(int i = 1; i <= 9; i++){
+		    solve(i,n);
+		}
+	}
+	public static void solve(int num, int n){
+	    if(num > n){
+	        return;
+	    }
+	    System.out.println(num);
+	    
+	    for(int digit = 0; digit <= 9; digit++){
+	        solve((num*10)+digit,n);
+	    }
+	}
+}
