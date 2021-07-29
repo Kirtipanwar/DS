ce923dc39b51d110a579e3f061fbf0ce903792ad
kirtipanwar <42113400+kirtipanwar@users.noreply.github.com>
2021-07-29 23:46:22 +0530

    Create diaBT.java

---
+++ L2/PRACTICE/TREES2/diaBT.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+  public static Scanner scn = new Scanner(System.in);
+
+  public static class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+  }
+
+  public static int diameterOfBinaryTree(TreeNode root) {
+    return -1;
+  }
+
+  public static TreeNode createTree(int[] arr, int[] IDX) {
+    if (IDX[0] > arr.length || arr[IDX[0]] == -1) {
+      IDX[0]++;
+      return null;
+    }
+
+    TreeNode node = new TreeNode(arr[IDX[0]++]);
+    node.left = createTree(arr, IDX);
+    node.right = createTree(arr, IDX);
+
+    return node;
+  }
+
+  public static void solve() {
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+      arr[i] = scn.nextInt();
+
+    int[] IDX = new int[1];
+    TreeNode root = createTree(arr, IDX);
+    System.out.println(diameterOfBinaryTree(root));
+  }
+
+  public static void main(String[] args) {
+    solve();
+  }
+}
